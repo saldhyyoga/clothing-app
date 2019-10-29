8632df5de1d53a309243d7835334ecbf767fb6df
saldhyyoga <saldhyy@gmail.com>
2019-10-30 01:47:13 +0700

    create homepage component&smaller component, styling with scss

---
+++ src/pages/homepage/homepage-component.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Directory from '../../components/directory/directory-menu'
+import './homepage.syle.scss'
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Directory />
+    </div>
+)
+
+export default HomePage;
